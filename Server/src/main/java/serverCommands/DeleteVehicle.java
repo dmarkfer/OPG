c0ba04e98196fa208f6568bb88b70b29f5a9c48f
Domagoj Markota <domagoj.markota@fer.hr>
2018-01-09 00:21:54 +0100

    Added DeleteVehicle functionality to backend service

--- Server/src/main/java/serverCommands/DeleteVehicle.java
+++ Server/src/main/java/serverCommands/DeleteVehicle.java
@@ -10,36 +10,34 @@ import serverShell.AbstractCommand;
 import serverShell.CommandStatus;
 import serverShell.Environment;
 
-public class EditComment extends AbstractCommand {
-
-	public EditComment() {
-		super("EDITCOMMENT", "Comment editing.");
+public class DeleteVehicle extends AbstractCommand {
+	
+	public DeleteVehicle() {
+		super("DELETEVEHICLE", "Delete vehicle.");
 	}
-
+	
 	@Override
 	public CommandStatus execute(Environment environment, JSONObject arguments) {
 		Connection connection = environment.getDatabase();
 		JSONObject returnObject = new JSONObject();
 		
-		try {						
+		try {
 			Statement statement = connection.createStatement();
 			
 			StringBuilder sql = new StringBuilder();
-			sql.append("UPDATE ocjena SET komenatar='");
-			sql.append(arguments.get("komentar"));
-			sql.append("', ocjena=");
-			sql.append(arguments.get("ocjena"));
-			sql.append(", vrijeme='");
-			sql.append(arguments.get("vrijeme"));
-			sql.append("';");
+			sql.append("DELETE FROM vehicle WHERE id=");
+			sql.append(arguments.get("idVozila"));
+			sql.append(";");
 			
-			statement.executeUpdate(sql.toString());
+			statement.execute(sql.toString());
+			
 			returnObject.put("success", true);
 			environment.sendText(returnObject.toString());
 		} catch (SQLException e) {
 			returnObject.put("success", false);
 			environment.sendText(returnObject.toString());
 		}
+		
 		return CommandStatus.CONTINUE;
 	}
 
