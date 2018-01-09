65333a4796f79e23fe28425419a2452f68ad587b
Domagoj Markota <domagoj.markota@fer.hr>
2018-01-09 01:00:41 +0100

    RetrieveVehicles functionality on backend service

---
+++ Server/src/main/java/serverCommands/RetrieveVehicles.java
@@ -0,0 +1,72 @@
+package serverCommands;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import serverShell.AbstractCommand;
+import serverShell.CommandStatus;
+import serverShell.Environment;
+
+public class RetrieveVehicles extends AbstractCommand {
+	
+	String[] dbColumns = new String[] {
+			"id",
+			"registarska_oznaka",
+			"slika_vozila",
+			"opis_vozila",
+			"id_kategorije_vozila"
+	};
+	
+	String[] dtoColumns = new String[] {
+			"idVozila",
+			"registarskaOznaka",
+			"slikaVozila",
+			"opisVozila",
+			"idKategorijeVozila"
+	};
+	
+
+	public RetrieveVehicles() {
+		super("RETRIEVEVEHICLES", "Retrieves vehicles.");
+	}
+
+	@Override
+	public CommandStatus execute(Environment environment, JSONObject arguments) {
+		Connection connection = environment.getDatabase();
+		JSONObject returnObject = new JSONObject();
+		
+		try {
+			Statement statement = connection.createStatement();
+			
+			StringBuilder sql = new StringBuilder();
+			
+			sql.append("SELECT * FROM vozilo WHERE id_korisnika=");
+			sql.append(arguments.get("idKorisnika"));
+			sql.append(";");
+			
+			ResultSet vehicles = statement.executeQuery(sql.toString());
+			
+			JSONArray vehs = new JSONArray();
+			while(vehicles.next()) {
+				JSONObject veh = new JSONObject();
+				for(int i=0; i<dbColumns.length; ++i) {
+					veh.put(dtoColumns[i], vehicles.getString(dbColumns[i]));
+				}
+				vehs.put(veh);
+			}
+			
+			returnObject.put("vozila", vehs);
+			
+			environment.sendText(returnObject.toString());
+		} catch (SQLException e) {
+			environment.sendText("false");
+		}
+		
+		return CommandStatus.CONTINUE;
+	}
+}
